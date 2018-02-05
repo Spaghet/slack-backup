7627b1b8f98831c8f616d6b6059e3c0b09f23f6c
sp4ghet <sp4ghet@users.noreply.github.com>
2018-02-05 12:10:53 +0900

    Use S3 to store zipped backup export file

--- fetch-messages.py
+++ fetch-messages.py
@@ -1,24 +1,52 @@
 #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import boto3
+
 import sys
 import os
+import glob
 
+# Get the config options for your slack user
 domain = 'http://{}.slack.com'.format(os.environ['SLACK_TEAM'])
 downloads = os.environ['SLACK_BACKUP_DIR'] # ~/Downloads
 username = os.environ['SLACK_USER']
 password = os.environ['SLACK_PASS']
 
-browser = webdriver.Chrome()
+# Get config for S3
+bucket_name = os.environ['BUCKET_NAME']
+
+# Set chromium options to run in headless mode
+options = Options()
+options.add_argument('--headless')
+
+# Set the download directory and other settings
+pref = {'download.default_directory': downloads,
+                     'download.prompt_for_download': False,
+                     'download.directory_upgrade': True,
+                     'safebrowsing.enabled': False,
+                     'safebrowsing.disable_download_protection': True}
+options.add_experimental_option('prefs', pref)
+
+browser = webdriver.Chrome(chrome_options=options)
+
+browser.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+
+# Download files when in headless mode
+params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': downloads}}
+command_result = browser.execute("send_command", params)
 
 try:
     print('opening page')
     browser.get('{}/services/export'.format(domain))
     browser.implicitly_wait(10)
+
+    # command_result = browser.execute('send_command', params)
 
     print('logging in')
     username_elem = browser.find_element_by_id('email')
@@ -50,7 +78,20 @@ try:
     latest_link.click()
 
     print('done')
-except:
+except Exception as e:
+    print(e)
     browser.close()
+
+# Wait until the download is completed
+files = []
+while len(files) == 0:
+    files = glob.glob('{}/*export*.zip'.format(downloads))
+
+newest = max(files, key=os.path.getctime)
+
+# Upload the file to S3
+s3 = boto3.resource('s3')
+with open(newest, 'rb') as file:
+    s3.Bucket(bucket_name).put_object(Key=newest, Body=file)
 
 browser.close()
--- slack-viewer.py
+++ slack-viewer.py
@@ -3,23 +3,37 @@ import glob
 import sys
 import os
 from slackviewer.main import main as slackviewer
+import boto3
 
-downloads = os.environ.get('SLACK_BACKUP_DIR') \
-    if os.environ.get('SLACK_BACKUP_DIR') is not None \
-    else "." # ~/Downloads
+# Get latest backup zip file from s3
+s3 = boto3.resource('s3')
 
+bucket_name = os.environ['BUCKET_NAME']
+backup_filename = 'backup.zip'
+
+filenames = []
+for objects in s3.Bucket(bucket_name).objects.filter():
+    filenames.append({'last_modified': objects.last_modified, 'key': objects.key})
+
+newest_s3_filename = max(filenames, key=lambda x: x['last_modified'])
+
+try:
+    s3.Bucket(bucket_name).download_file(newest_s3_filename, backup_filename)
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
+
+# Set up slack-export-viewer server
 port = os.environ.get('PORT') \
     if os.environ.get('PORT') is not None \
     else 5000
 slackviewer_path = '$(which slack-export-viewer)'
 
-files = glob.glob('{}/*export*.zip'.format(downloads))
-newest = max(files, key=os.path.getctime)
-print(newest)
-
 sys.argv[0] = slackviewer_path
 sys.argv.append('-z')
-sys.argv.append(newest)
+sys.argv.append(backup_filename)
 sys.argv.append('-p')
 sys.argv.append(port)
 sys.argv.append('--no-browser')
